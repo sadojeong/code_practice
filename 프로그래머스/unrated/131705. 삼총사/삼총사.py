1e3fec45a09c74639c321aa3b820e1e171fda263
sadojeong <87840004+sadojeong@users.noreply.github.com>
2022-10-15 19:40:24 +0900

    [unrated] Title: 삼총사, Time: 0.03 ms, Memory: 9.86 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/131705. 삼총사/삼총사.py
@@ -0,0 +1,9 @@
+from itertools import combinations
+
+def solution(number):
+    count = 0
+    for comb in combinations(number, 3):
+        if sum(comb) == 0:
+            count += 1
+            
+    return count
