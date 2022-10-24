e7c15b6e3549de42a607bf5bff522080c4b3b477
sadojeong <87840004+sadojeong@users.noreply.github.com>
2022-10-24 18:43:25 +0900

    [level 2] Title: 전화번호 목록, Time: 108.99 ms, Memory: 28.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,6 +1,6 @@
 def solution(phone_book):
     answer = True
-    phone_book_sorted = sorted(phone_book, key = lambda x: (x, len(x)))
+    phone_book_sorted = sorted(phone_book, key = lambda x: (x))
 
     for i in range(1, len(phone_book_sorted)):
         if phone_book_sorted[i-1] == phone_book_sorted[i][:len(phone_book_sorted[i-1])]:
